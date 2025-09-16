dd0c90321181ec60f9c4e19f7a8368a9a879f53b
unibox <unibox@duck.com>
2025-09-16 11:43:57 +0200

    Initial commit (via bun create)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://example.com',
+
   adapter: node({
     mode: 'standalone',
   }),
+
   output: 'server',
+  integrations: [sitemap()],
 });
