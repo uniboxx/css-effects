e603d5aa815f2706ed49a87fc789d166219ec764
unibox <unibox@duck.com>
2025-09-27 18:11:25 +0200

    sez 08 fill-text effect

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://unibox-css-effects.netlify.app/',
-
   integrations: [sitemap()],
+  adapter: netlify(),
 });
